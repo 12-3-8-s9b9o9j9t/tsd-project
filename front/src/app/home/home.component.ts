0fbf50eff60c9bcfc5d43065b7bd5915bebdbc1a
tpicaud <tom.picaud@etudiant.univ-rennes1.fr>
2023-03-23 00:17:25 +0100

    update home and session component

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  nameControl = new FormControl('', [Validators.required]);
+
+  formGroup = new FormGroup({
+    name: this.nameControl,
+  });
+
+  constructor(   
+    private router: Router
+  ) { }
+
+
+  enter(): void {
+    this.router.navigateByUrl('/session');
+  }
+
 
 }
