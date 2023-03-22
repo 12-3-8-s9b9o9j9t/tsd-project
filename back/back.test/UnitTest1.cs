7edd16ce8524e6ecf162a51d6ea1a2fdf992e6b1
yanis <yanis.bouger@gmail.com>
2023-03-22 19:07:51 +0100

    added dummy test to check action

--- back/back.test/UnitTest1.cs
+++ back/back.test/UnitTest1.cs
@@ -4,8 +4,9 @@ namespace back.test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void dummyTest()
         {
+            Assert.IsTrue(true);
         }
     }
 }
