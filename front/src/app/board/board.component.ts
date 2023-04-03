72e06aaf97dfdffd0ca872af6a2522d3246da6d4
tpicaud <tom.picaud@etudiant.univ-rennes1.fr>
2023-04-03 20:32:19 +0200

    update changes

--- front/src/app/board/board.component.ts
+++ front/src/app/board/board.component.ts
@@ -1,16 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiHelperService } from '../services/api-helper.service';
+import { getName } from '../services/storage.service';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent {
+export class BoardComponent implements OnInit {
 
-  players: string[] = ['Yanis','Hector',"Jack","Georges"];
-  player_cards: string[] | undefined;  // To be filled by the server
+  players: Player[] = []; // To be filled by the server
+  showCards: boolean = false;
 
   constructor(private api: ApiHelperService) { }
 
+  ngOnInit(): void {
+    this.refreshBoard();
+  }
+
+  refreshBoard() {
+    //refresh player  & player cards every 1 second
+    this.api.get({ endpoint: '/Session' }).then((response) => {
+      this.players = response.users.map((user: any) => {
+        if (user.name != getName() && !this.players.includes(user.name)) {
+          this.players.push(new Player(user.name));
+        }
+      });
+      if (response.state == "discussing") {
+        this.showCards = true;
+        // update player card for each player
+        this.players.forEach((player) => {
+          response.users.forEach((user: any) => {
+            if (user.name == player.name) {
+              player.card = user.card;
+            }
+          });
+        });
+      }
+    }
+    ).catch((error) => {
+      console.log(error);
+      console.log("Error getting session");
+    }
+    );
+    setTimeout(() => { this.refreshBoard(); }, 1000);
+  }
+}
+
+class Player {
+  name: string;
+  card: string | undefined;
+
+  constructor(name: string) {
+    this.name = name;
+    this.card = undefined;
+  }
 }
--- front/src/app/waiting-room/waiting-room.component.ts
+++ front/src/app/waiting-room/waiting-room.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
 import { ApiHelperService } from '../services/api-helper.service';
-import { getName } from '../services/storage.service';
+import { getID, getName } from '../services/storage.service';
 
 @Component({
   selector: 'app-waiting-room',
@@ -13,37 +13,73 @@ export class WaitingRoomComponent implements OnInit {
 
   displayedColumns: string[] = ['name', 'status'];
   player: Player;
-  currentPlayers: Player[] = []//[new Player("John"), new Player("Jane"), new Player("Jack")];
+  currentPlayers: Player[] = [];
 
-  constructor(private api: ApiHelperService, private router: Router) { 
+  constructor(private api: ApiHelperService, private router: Router) {
     this.player = new Player(getName());
   }
 
   ngOnInit(): void {
     this.player = new Player(getName());
-    this.currentPlayers.push(this.player);
     this.refreshCurrentPlayers();
+
+    // post this user in the session
+    this.api.post({ endpoint: '/Session/addUser/' + getID() }).then((response) => {
+      console.log("User added to session");
+    }).catch((error) => {
+      console.log(error);
+      console.log("Error adding user to session");
+    });
   }
 
   setReady() {
+    this.api.post({ endpoint: '/Session/start/' + getID() }).then((response) => {
+      
+      console.log(response);
+      console.log("User ready");
+    }).catch((error) => {
+      console.log(error);
+      console.log("Error sending ready");
+    });
+
     this.player.isPlayerReady = true;
   }
 
   setNotReady() {
+    // this.api.post({ endpoint: '/Session/stop/' + getID() }).then((response) => {
+    //   console.log("User not ready");
+    // }).catch((error) => {
+    //   console.log(error);
+    //   console.log("Error sending not ready");
+    // });
+
     this.player.isPlayerReady = false;
   }
 
   refreshCurrentPlayers() {
     // refresh every 1 second
 
-    // if all players are ready, redirect to game page
-    if (this.currentPlayers.every(player => player.isPlayerReady)) {
-      this.router.navigateByUrl('/session')
-    } else {
-      // get current players from server
+    // Get players in the session
+    this.api.get({ endpoint: '/Session' }).then((response) => {
+      this.currentPlayers = []
+      response.users.forEach((user: any) => {
+        this.currentPlayers.push(new Player(user.name));
+      });
+      console.log(this.currentPlayers);
+      // update user status for each player
 
-    }
-    setTimeout(() => { this.refreshCurrentPlayers(); } ,1000);
+      // if session state is "voting", redirect to session page
+      if (response.state == "voting") {
+        this.router.navigateByUrl('/Session');
+      }
+
+    }).catch((error) => {
+      console.log(error);
+      console.log("Error getting session");
+    });
+
+
+    setTimeout(() => { this.refreshCurrentPlayers(); }, 1000);
   }
 }
 
