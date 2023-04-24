53873de69eef9cc788f9cbf917e49fd5afa068b1
tpicaud <tom.picaud@etudiant.univ-rennes1.fr>
2023-04-24 11:06:10 +0200

    add go home button

--- front/src/app/end-page/end-page.component.ts
+++ front/src/app/end-page/end-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-end-page',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class EndPageComponent {
 
+  constructor(private router: Router) { }
+
+  onHomeClick(): void {
+    this.router.navigate(['/home']);
+  }
 }
