ffeb13ef5e958ef775414f8af3ae008b2f729fed
tpicaud <tom.picaud@etudiant.univ-rennes1.fr>
2023-04-27 00:38:10 +0200

    change url to localhost

--- front/src/app/environments/environment.ts
+++ front/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    api_url: 'http://192.168.1.4:5225',
-    ws_url: 'ws://192.168.1.4:5225/ws'
+    api_url: 'http://localhost:5225',
+    ws_url: 'ws://localhost:5225/ws'
 }
