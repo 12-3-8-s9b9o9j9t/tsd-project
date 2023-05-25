ef2dac8f8942d3e3fde4b9af862d4e8db4c5701f
tpicaud <tom.picaud@etudiant.univ-rennes1.fr>
2023-05-25 14:49:26 +0200

    download recap

--- front/src/app/end-page/end-page.component.ts
+++ front/src/app/end-page/end-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiHelperService } from '../services/api-helper.service';
+import { getSessionIdentifier } from '../services/storage.service';
 
 @Component({
   selector: 'app-end-page',
@@ -14,4 +15,28 @@ export class EndPageComponent {
   onHomeClick(): void {
     this.router.navigate(['/home']);
   }
+
+  async downloadRecap() {
+    try {
+      const response = await this.api.get({ endpoint: '/Session/' + getSessionIdentifier() + '/download', responseType: 'text' });
+      const blob = new Blob([response], { type: 'application/csv' });
+      const url = window.URL.createObjectURL(blob);
+  
+      // Nom du fichier
+      const fileName = 'recap_'+getSessionIdentifier()+'.csv';
+  
+      // Création de l'élément <a> avec le lien de téléchargement
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = fileName;
+  
+      // Ajout de l'élément <a> à la page et déclenchement du téléchargement
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
 }
