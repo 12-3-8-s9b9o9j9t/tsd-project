33de986544b1cff3acdc8e23b0faaf645fd474cd
yanis <yanis.bouger@gmail.com>
2023-05-28 12:43:43 +0200

    migration

---
+++ back/back.web/Migrations/20230528104332_Migration4.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace back.web.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "password",
+                table: "user",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "password",
+                table: "user");
+        }
+    }
+}
