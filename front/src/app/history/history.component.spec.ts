eb90c3ba0a628fc5bb73faa926a1e9127b7b1774
yanis <yanis.bouger@gmail.com>
2023-05-29 13:02:08 +0200

    test front pass

--- front/src/app/history/history.component.spec.ts
+++ front/src/app/history/history.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HistoryComponent } from './history.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('HistoryComponent', () => {
   let component: HistoryComponent;
@@ -8,7 +11,12 @@ describe('HistoryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HistoryComponent ]
+      declarations: [ HistoryComponent ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MatIconModule
+      ]
     })
     .compileComponents();
 
