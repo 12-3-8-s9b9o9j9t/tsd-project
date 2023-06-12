deb94c75f867059049980d485bf9999ce7495b9c
yanis <yanis.bouger@gmail.com>
2023-06-12 16:35:57 +0200

    fix tests

--- front/src/app/session/session.component.spec.ts
+++ front/src/app/session/session.component.spec.ts
@@ -9,6 +9,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
 
 describe('SessionComponent', () => {
   let component: SessionComponent;
@@ -26,7 +27,8 @@ describe('SessionComponent', () => {
         MatFormFieldModule,
         ReactiveFormsModule,
         MatInputModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        MatDialogModule
       ]
     })
     .compileComponents();
